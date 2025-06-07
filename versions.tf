2e567bbdfe69a2eb2c4aec00bc2a2af6985be9be
SSEEIIKKOO <seikohaibara@gmail.com>
2025-06-07 11:11:39 +0900

    Revert "PR作るために消した"
    This reverts commit e5ce86b0136d7b2d664ecb57383402356c792ae6.

---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+
+    required_version = "1.11.4"
+    required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = "5.97.0"
+    }
+    }
+    backend "s3"{
+    bucket = "kdg-aws-2025-sseeiikkoo"
+    key    = "terraform/state.tfstate"
+    region = "ap-northeast-1"
+    encrypt = true
+    }
+}
+
+provider "aws" {
+    region  = "ap-northeast-1"
+}
